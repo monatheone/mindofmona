2a2f2964da188d4ead0fa6731c87b23672ac2636
shahan27 <shahan27@live.com>
2023-03-05 19:00:38 -0500

    Added new files and edited landing page

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 portfolio
 
 
-Hi my name is mona and i like it up the butt! thanks for stopping by
+Hi my name is mona!
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,7 +13,7 @@ class Header extends Component {
 
     return (
       <header id="home">
-        <ParticlesBg type="circle" bg={true} />
+        <ParticlesBg type="square" bg={true} />
 
         <nav id="nav-wrap">
           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
@@ -62,10 +62,11 @@ class Header extends Component {
               <h1 className="responsive-headline">{name}</h1>
             </Fade>
             <Fade bottom duration={1200}>
-              <h3>{description}.</h3>
+              {/*<h3>{description}.</h3>*/}
             </Fade>
             <hr />
             <Fade bottom duration={2000}>
+              {/* 
               <ul className="social">
                 <a href={project} className="button btn project-btn">
                   <i className="fa fa-book"></i>Project
@@ -74,6 +75,7 @@ class Header extends Component {
                   <i className="fa fa-github"></i>Github
                 </a>
               </ul>
+              */}
             </Fade>
           </div>
         </div>
